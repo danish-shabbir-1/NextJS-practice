a23f482cbf7c23794aacd0bd411dc60b9bedd3e2
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-14 05:13:18 -0700

    practice....

--- nextjs-practice/src/app/page.js
+++ nextjs-practice/src/app/page.js
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Dashboard from "./Dashboard/page";
 
 export default function Home() {
   return (
@@ -37,6 +38,7 @@ export default function Home() {
           height={37}
           priority
         />
+            <Dashboard />
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
