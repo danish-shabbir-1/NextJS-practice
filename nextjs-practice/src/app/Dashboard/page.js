ede1c0f08b00fa52f13ac056519302251d316f32
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-14 05:13:10 -0700

    practice....

--- nextjs-practice/src/app/Dashboard/page.js
+++ nextjs-practice/src/app/Dashboard/page.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-const dashboard = () => {
+const Dashboard = () => {
+
+fetch('https://dummyjson.com/products')
+.then(res => res.json())
+.then(res => console.log(res))
+
   return (
     <div>
-      
     </div>
   )
 }
 
-export default dashboard
+export default Dashboard
