b572f4332711c943e4a782085ef57f175dba86aa
DANISH SHABBIR <danishshabbir290@gmail.com>
2024-02-15 06:09:38 -0700

    practice

--- nextjs-practice/src/app/Dashboard/page.js
+++ nextjs-practice/src/app/Dashboard/page.js
@@ -2,7 +2,7 @@ import Detail from "./Detail"
 
 const Dashboard = async () => {
 
-const res = await fetch('https://dummyjson.com/products')
+const res = await fetch('https://api.imgflip.com/get_memes')
 const result = await res.json()
 
 console.log(result);
